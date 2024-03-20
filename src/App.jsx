78762ab92f0be49a7e2e7666e1042ccf25e3387b
hazrat120 <125098771+hazrat120@users.noreply.github.com>
2024-03-20 21:48:33 +0600

    add css design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Logo from "./component/Logo";
+import "./assets/css/global.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <h1>Learn With Sumit</h1>
       <Logo />
     </div>
